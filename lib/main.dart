a7fb4a467302050a49a605f90517938f3ce1ce7c
trangiatiendat <tgtd2k3@gmail.com>
2024-10-03 01:24:39 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+// Creates a Material App
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: BallPage(),
+  ),
+);
+
+// Creates a Scaffold with
+// appbar using Stateless widget
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('GeeksforGeeks'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+// Creates a Stateful widget
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            // Random.nextInt(n) returns a random integer from 0 to n-1
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        // Adding images
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
